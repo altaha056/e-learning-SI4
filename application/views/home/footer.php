3024183d35ee29e41568ecc1d08449ecd8bd74e1
Altaha <altahabinsyafriadi@gmail.com>
2020-04-25 06:55:55 +0700

    TUGAS UTS SI 4

---
+++ application/views/home/footer.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+          <footer class="footer text-center mt-5 mb-5" id="hak">
+              <img src="assets/usu.png" width=55px class="mr-5">E-Learning USU<hr>&copy;2020 All Rights Reserved</footer>
+</body>
+</html>
---
+++ application/views/home/home.php
@@ -0,0 +1,313 @@
+
+<!-- ALTAHA, HADY, MITA -->
+<!-- UTS_SI4_056_092_098-->
+<!-- UTS_SI4_056_092_098-->
+<!-- UTS_SI4_056_092_098-->
+<!-- UTS_SI4_056_092_098-->
+<!-- UTS_SI4_056_092_098-->
+<!-- UTS_SI4_056_092_098-->
+<!-- UTS_SI4_056_092_098-->
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>E-Learning USU</title>
+        <link rel="icon" href="assets/usu.png">
+    </head>
+    
+    <body>
+        <!--header start-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a class="navbar-brand" href="#"><img src="assets/usu.png" style="max-height: 44px"></a>
+            <a class="navbar-brand" href="#"><h3>E-Learning</h3></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Utama</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#jadwal">Jadwal</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Layanan
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#ipk">Hitung IPK</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#fasilitas">Fasilitas</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#peta">Lokasi</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#hak">Hak Cipta</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Kom B Ilmu Komputer</a>
+              </li>
+            </ul>
+                <form class="form-inline my-2 my-lg-0">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit" data-toggle="modal" data-target="#exampleModalCenter">Daftar Tugas</button>
+                </form>
+          </div>
+        </nav>
+        <!--header end-->
+        
+              <button type="button" class="btn btn-primary mx-auto" data-toggle="modal" data-target="#exampleModalCenter">Daftar Tugas</button>
+        
+        
+        
+        <!--landing page start-->
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" >
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="assets/bg1.jpg" class="d-block w-100">
+              <div class="carousel-caption">
+                <h1>Rumahmu dibalik gunung?</h1>
+                <p>Tak perlu datang ke kampus. Pakai E-Learning saja.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="assets/bg2.jpg" class="d-block w-100">
+              <div class="carousel-caption">
+                <h1>Harus melewati badai untuk pergi ke kampus?</h1>
+                <p>Tak perlu bersusah payah. Pakai E-Learning saja.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="assets/bg3.jpg" class="d-block w-100">
+              <div class="carousel-caption">
+                <h1>Gunung meletus?</h1>
+                <p>Tak perlu khawatir. Pakai E-Learning saja.</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <!--landing page end-->
+        
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Daftar Tugas Kom B Ilmu Komputer USU</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <table class="table table-borderless">
+                      <thead>
+                        <tr>
+                          <th scope="col"></th>
+                          <th scope="col">Tugas</th>
+                          <th scope="col">Pengumpulan</th>
+                          <th scope="col">Deadline</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th>KDJK</th>
+                          <td>Kerjakan tugas di e-learning</td>
+                          <td>E-learning USU</td>
+                          <td>12 April, 16.00 WIB</td>
+                        </tr>
+                        <tr>
+                          <th>SI</th>
+                          <td>Merampungkan database projek</td>
+                          <td>E-mail: elvischan@gmail.com</td>
+                          <td>14 April, 10.20 WIB</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--tugas-->
+        <!--schedule start-->
+        <div class="jumbotron" id="jadwal">
+            <div class="container">
+            <div class="col-lg-12 text-center mt-5 mb-5">
+                <h3 class="display-4">Jadwal Kuliah</h3>
+            </div>
+            <table class="table table-striped" style="text-align: center">
+            <thead class="table-dark">
+                <th>Waktu</th>
+                <th>Senin</th>
+                <th>Selasa</th>
+                <th>Rabu</th>
+                <th>Kamis</th>
+                <th>Jumat</th>
+            </thead>
+            <tr>
+                <th>08.00 - 09.40</th>
+                <td>Kecakapan<br>Antar<br>Personal</td>
+                <td></td>
+                <td>Komunikasi<br>dan Jaringan<br>Komputer</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>09.40 - 11.20</th>
+                <td></td>
+                <td>Sistem<br>Informasi</td>
+                <td></td>
+                <td>Organisasi<br>dan Arsitektur<br>Komputer</td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>11.20 - 13.00</th>
+                <td>Teori<br>Bahasa<br>Otomata</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>13.00 - 14.40</th>
+                <td></td>
+                <td>Pengajaran<br>Berbantuan<br>Komputer</td>
+                <td></td>
+                <td></td>
+                <td>Pemrograman<br>Visual</td>
+            </tr>
+            <tr>
+                <th>14.40 - 16.40</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Komputer<br>Grafik<br>Visual</td>
+                <td></td>
+            </tr>
+        </table>
+        </div>
+            
+            <!--tugas-->
+
+
+            
+        </div>        
+        <!--schedule start-->
+     
+        <script>
+            function valiDasi() {
+                var x, text;
+                x = document.getElementById("angka").value;
+                if ( x < 0 || x > 4 || isNaN(x)) {
+                    text = "Input tidak sesuai<br><h6>gunakan koma untuk memisahkan desimal</h6>";
+                }
+                    else if (x>=3.5){
+                    text = "Selamat Anda mendapatkan cumlaude"
+                }
+                    else if (x>=3){
+                    text = "Anda lulus dengan nilai A"
+                }
+                    else if (x>=2){
+                    text = "Anda lulus dengan nilai B"
+                }
+                    else if (x>=1){
+                    text = "Anda lulus dengan nilai C"
+                }
+                    else if (x>=0){
+                    text = "Anda tidak lulus"
+                }
+              document.getElementById("hasil").innerHTML = text;
+        }
+        </script>
+        
+        <!--bacot start-->
+
+        
+        <div id="ipk" class="jumbotron" style="background-image: url(assets/bg5.jpg); background-size: cover">
+            <h3 style="color: white" class="text-center display-3">Hitung Akreditasi IPK</h3>
+            <div class="input-group mb-5 pb-5 mt-5 pt-5 mx-auto" style="width: 444px;">
+              <input type="text" class="form-control" placeholder="Hitung IPK Mahasiswa" aria-label="Recipient's username" aria-describedby="button-addon2"  id="angka">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2" onclick="valiDasi()" style="color: white">Hitung</button>
+              </div>
+            </div>
+            <div class="col-lg-12 text-center">
+                <h3 id="hasil" style="color: white"></h3>
+            </div>
+        </div> 
+        
+        <div class="container-fluid pt-5 mt-5" id="fasilitas">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h3 class="display-3">Fasilitas</h3>
+            </div>
+        </div>
+        <div class="row mt-5">
+        <div class="col-lg-4 col-md-6">
+            <div class="card" >
+              <img src="assets/Rumah-Sakit-USU-Medan.png" class="card-img-top" alt="rumah sakit usu">
+              <div class="card-body">
+                <h5 class="card-title">Rumah Sakit USU</h5>
+                <p class="card-text">Rampung pada tahun 2016, berlokasi tepat didepan USU. Menjadi objek vital bagi mahasiswa kedokteran USU.</p>
+                <a target="_blank" href="http://rumahsakit.usu.ac.id/index.php/id/contact" class="btn btn-primary stretched-link">Kunjungi</a>
+              </div>
+            </div>
+        </div>        
+        <div class="col-lg-4 col-md-6">
+            <div class="card" >
+              <img src="assets/perpustakaan.jpg" class="card-img-top" alt="perpustakaan usu">
+              <div class="card-body">
+                <h5 class="card-title">Perpustakaan USU</h5>
+                <p class="card-text">Perpustakaan yang sudah menggunakan program tersistem untuk peminjaman, administrasi, dan pencarian melalui OPAC.</p>
+                <a target="_blank" href="https://digilib.usu.ac.id/" class="btn btn-primary stretched-link">Kunjungi</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+            <div class="card" >
+              <img src="assets/tamanrusa.jpg" class="card-img-top" alt="taman birek">
+              <div class="card-body">
+                <h5 class="card-title">Taman Birek</h5>
+                <p class="card-text">Hamparan rumput yang asri ditambah koleksi satwa membuat hari terpesona sejauh mata memandang. Rusa, angsa, dan masih banyak hewan lainnya.</p>
+                <a target="_blank" href="https://goo.gl/maps/qsdvnTNZPbVZVUHH9" class="btn btn-primary stretched-link">Lihat</a>
+              </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        <!--bacot end-->
+     
+        <!--peta start-->
+        <div class="embed-responsive embed-responsive-16by9 mt-5" id="peta">
+            <div class="container-fluid">
+                <div id="map-container-google-1">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.1080659493414!2d98.65757021414561!3d3.5625862514668705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30312fdf9ea854fb%3A0x16d8d4e91cbc241d!2sProgram%20Studi%20S1%20Ilmu%20Komputer%20Universitas%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1587730175332!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+            </div>
+        </div>
+        <!--peta akhir-->
+        
+        <!---footer awal-->
+
+        <!---footer akhir-->
+    </body>
+</html>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
